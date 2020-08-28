b0a5a1360c5f88b68176320652cbe10d5817af8b
Tymur Foshch <antariuz@gmail.com>
2020-08-28 22:24:19 +0300

    "Working on' commit

--- src/best/com/ArrayList.java
+++ src/best/com/ArrayList.java
@@ -2,4 +2,52 @@ package best.com;
 
 public class ArrayList {
 
+    private Object E;
+    private final int INITIAL_CAPACITY = 10;
+    private int currentCapacity;
+    private int index;
+
+
+    public ArrayList(){
+        this.ArrayList
+    }
+
+
+//    public void add(Object E) {
+//        Object[currentCapacity] = new Object[currentCapacity + 1];
+//        ArrayList[currentCapacity]
+//    }
+
+    public Object getE() {
+        return E;
+    }
+
+    public void setE(Object e) {
+        E = e;
+    }
+
+    public int getINITIAL_CAPACITY() {
+        return INITIAL_CAPACITY;
+    }
+
+    public void setINITIAL_CAPACITY(int INITIAL_CAPACITY) {
+        this.INITIAL_CAPACITY = INITIAL_CAPACITY;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+
 }
