2c7652f02ba1a349c17e1f0227a10440b7a28b7e
Tymur Foshch <antariuz@gmail.com>
2020-09-02 00:09:19 +0300

    Working on MyLinkedList

--- src/best/com/Main.java
+++ src/best/com/Main.java
@@ -1,6 +1,5 @@
 package best.com;
 
-
 public class Main {
 
     public static void main(String[] args) {
--- src/best/com/MyLinkedList.java
+++ src/best/com/MyLinkedList.java
@@ -5,18 +5,15 @@ public class MyLinkedList<E> {
     private MyNode<E> head;
     private MyNode<E> tail;
 
-    private int size;
-
     public MyLinkedList() {
     }
 
     public void add(E e) {
-        MyNode<E> node = new MyNode(e);
-        node.setItem(e);
-        if (node.getPrev() == null){
-            node = head;
-        } else if (node.getNext() == null){
-            node = tail;
+        MyNode<E> node = new MyNode<>(e);
+        if (head == null){
+            head = tail = node;
+        } else {
+
         }
     }
 
